c08d63703149b2cdb982df0568d517a44870b46b
Filipa Martins <FIL@MBP-de-Filipa-4.lan>
2016-11-05 17:45:44 +0000

    1

---
+++ main.js
@@ -0,0 +1,14 @@
+var headerHeight = $('.prez').outerHeight();
+var marNegSearch = parseInt($('.search-field').css('margin-top'));
+var calc = headerHeight + marNegSearch;
+
+$(window).on("scroll", function(e) {
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop > calc) {
+    $('.nav').css("position","fixed");
+    $('.nav').css("margin", "0");
+  } else {
+    $('.nav').removeClass("fix-search");
+  }
+
+});
---
+++ reservas.php
@@ -0,0 +1,26 @@
+<?php
+            if(!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['texto']) && $pValue){
+                $assunto = $_POST['nome'] . " - reservas";
+                $corpo = "De: " . $_POST['nome'] . "<" . $_POST['email'] . ">\n\n reserva para " $pessoas "pessoas". $_POST['texto'];
+                send_email("matilde.branco96@gmail.com",$assunto, $corpo);
+                send_email("filsun6@gmail.com",$assunto, $corpo);
+            }
+
+            function send_email($para, $assunto, $corpo){
+                if (mail($para, $assunto, $corpo)) {
+                    echo("<script> alert('Mensagem pronta para envio!');
+history.back() </script>");
+                } else {
+                    echo("<script> alert('Falha no envio!!'); history.back() </script>");
+                }
+            }
+
+            if(isset($_POST['search']))
+{
+
+    $pValue = $_POST['NPessoas']; // make value
+
+    $pessoas = mysql_real_escape_string($_POST['selected_text']); // get the selected text
+    echo $pessoas;
+}
+        ?>

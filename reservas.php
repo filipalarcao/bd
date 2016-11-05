973bccf557e48b7f9913882954aa0e91be4b4297
Filipa Martins <FIL@MBP-de-Filipa-4.lan>
2016-11-05 17:51:47 +0000

    4

--- reservas.php
+++ reservas.php
@@ -3,7 +3,6 @@
                 $assunto = $_POST['nome'] . " - reservas";
                 $corpo = "De: " . $_POST['nome'] . "<" . $_POST['email'] . ">\n\n reserva para " $pessoas "pessoas". $_POST['texto'];
                 send_email("matilde.branco96@gmail.com",$assunto, $corpo);
-                send_email("filsun6@gmail.com",$assunto, $corpo);
             }
 
             function send_email($para, $assunto, $corpo){
@@ -15,12 +14,9 @@ history.back() </script>");
                 }
             }
 
-            if(isset($_POST['search']))
-{
-
     $pValue = $_POST['NPessoas']; // make value
 
     $pessoas = mysql_real_escape_string($_POST['selected_text']); // get the selected text
     echo $pessoas;
-}
+
         ?>

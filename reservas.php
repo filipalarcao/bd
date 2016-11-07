e7939b6585c58ab020036cea3bb03463c4237990
matildebranco <matilde.branco96@gmail.com>
2016-11-07 14:45:14 +0000

    alteração do php
    falta colocar o teu mail

--- reservas.php
+++ reservas.php
@@ -1,22 +1,25 @@
 <?php
-            if(!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['texto']) && !empty($_POST['NPessoas'])){
+            
+
+            $pValue = $_POST['NPessoas']; // make value
+
+
+
+            if(!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['texto']) && !empty($pValue)){
+                
+                $pessoas = mysql_real_escape_string($_POST['selected_text']); // get the selected text
                 $assunto = $_POST['nome'] . " - reservas";
-                $corpo = "De: " . $_POST['nome'] . "<" . $_POST['email'] . ">\n\n reserva para " $pessoas "pessoas". $_POST['texto'];
+                $corpo = "De: " . $_POST['nome'] . "<" . $_POST['email'] . ">\n\n Reserva em nome de " . $_POST['nome'] . " para mesa de " . $pessoas . " pessoas. \n Observações: ". $_POST['texto'];
                 send_email("matilde.branco96@gmail.com",$assunto, $corpo);
             }
 
             function send_email($para, $assunto, $corpo){
                 if (mail($para, $assunto, $corpo)) {
-                    echo("<script> alert('Mensagem pronta para envio!');
-history.back() </script>");
+                    echo("<script> alert('Mensagem pronta para envio!'); history.back() </script>");
                 } else {
                     echo("<script> alert('Falha no envio!!'); history.back() </script>");
                 }
             }
 
-    $pValue = $_POST['NPessoas']; // make value
-
-    $pessoas = mysql_real_escape_string($_POST['selected_text']); // get the selected text
-    echo $pessoas;
 
         ?>

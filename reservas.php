e20d998bac6aca9fe5288d03c2e23395204edd40
Filipa Martins <FIL@MBP-de-Filipa-4.lan>
2016-12-07 22:35:23 +0000

    ºip0ug

--- reservas.php
+++ reservas.php
@@ -1,10 +1,10 @@
 <?php
-            
+
 
 
 /*
             if(!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['texto']) && !empty($_POST['NPessoas'])){
-                
+
                 $pessoas = mysql_real_escape_string($_POST['selected_text']); // get the selected text
                 $assunto = $_POST['nome'] . " - reservas";
                 $corpo = "De: " . $_POST['nome'] . "<" . $_POST['email'] . ">\n\n Reserva em nome de " . $_POST['nome'] . " para mesa de " . $pessoas . " pessoas. \n Observações: ". $_POST['texto'];
@@ -21,11 +21,11 @@
 
 */
             if(!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['texto']) ){
-                
+
                // $pessoas = mysql_real_escape_string($_POST['selected_text']); // get the selected text
                 $assunto = $_POST['nome'] . " - reservas";
                 $corpo = "De: " . $_POST['nome'] . "<" . $_POST['email'] . ">\n\n Reserva em nome de " . $_POST['nome'] . " para mesa de  pessoas. \n Observações: ". $_POST['texto'];
-                send_email("matilde.branco96@gmail.com",$assunto, $corpo);
+                send_email("filsun6@gmail.com",$assunto, $corpo);
             }
 
             function send_email($para, $assunto, $corpo){
